40d704c2a441a74c8b2f766695f32e9b15ea8a17
Paco Cid <paco.cid@kaplan.com>
2025-07-29 13:55:49 +0200

    removed log from Channel Page available test

--- tests/channel-page-available.test.js
+++ tests/channel-page-available.test.js
@@ -8,16 +8,13 @@ fixture('TV OS - Channel Page Available')
 
 test('Verify Channels page is available to use', async t => {
     // ACT: Robust navigation to the channels page
-   
-    // Log inicial
     const urlBefore = await getCurrentUrl();
-    console.log('URL antes de navegar a Channels:', urlBefore);
-    await t.expect(true).ok(`URL antes de navegar: ${urlBefore}`);
+    await t.expect(true).ok(`URL before navigating to Channels: ${urlBefore}`);
     await HomePage.navigateToChannels();
 
     const urlAfter = await getCurrentUrl();
 
-    console.log('URL after navigating to Channels:', urlAfter);
+    //console.log('URL after navigating to Channels:', urlAfter);
 
     await t.expect(urlAfter).contains('channels', 'URL should indicate the channels page');
 
